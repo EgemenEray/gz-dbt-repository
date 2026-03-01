fdd83b25c58e8fa39532d4c57f861abb4da5e82e
Egemen Eray Erdoğdu <egemenerayerdogdu@gmail.com>
2026-03-01 20:10:04 +0000

    finance-campaign

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,7 @@
+select * from {{ ref("stg_raw_adwords")}}
+UNION ALL
+select * from {{ ref("stg_raw_bing")}}
+UNION ALL
+select * from {{ ref("stg_raw_criteo")}}
+UNION ALL
+select * from {{ ref("stg_raw_facebook")}}
---
+++ models/mart/finance/finance_campaign_days.sql
@@ -0,0 +1,19 @@
+select
+    date_date,
+    operational_margin-ads_cost as ads_margin,
+    round(average_basket,2) as average_basket,
+    operational_margin,
+    ads_cost,
+    ads_impression,
+    ads_clicks,
+    quantity,
+    revenue,
+    purchase_cost,
+    margin,
+    shipping_fee,
+    log_cost,
+    ship_cost
+from {{ref('int_campaigns_day')}}
+FULL OUTER JOIN {{ref('finance_days')}}
+USING date_date
+order by date_date DESC
